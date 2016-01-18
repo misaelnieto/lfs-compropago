fa04684f6b2faa6aa6faff12985af841ba4236c3
tzicatl <nnieto@noenieto.com>
2016-01-17 21:10:57 -0800

    Make this work with api versions 1.0 and 1.1

--- lfs_compropago/views.py
+++ lfs_compropago/views.py
@@ -13,7 +13,7 @@ from django.views.decorators.http import require_POST
 from dateutil.parser import parse as parse_date
 
 from lfs.core.signals import order_paid as lfs_order_paid_signal
-from lfs.order.settings import PAID
+from lfs.order.settings import PAID, PAYMENT_FAILED
 from lfs.mail import utils as mail_utils
 from lfs.core.utils import get_default_shop
 
@@ -41,14 +41,45 @@ def transaction_view(request, tx_id):
             payment_type = txn.payment_type
         )
         r = api.charge(c)
-        txn.payment_id = r['payment_id']
-        txn.short_payment_id = r['short_payment_id']
-        txn.payment_status = r['payment_status']
-        txn.creation_date = parse_date(r['creation_date'], ignoretz=True)
-        txn.expiration_date = parse_date(r['expiration_date'], ignoretz=True)
-        txn.updated_date = datetime.now()
-        txn.product_information = json.dumps(r['product_information'])
-        txn.payment_instructions = json.dumps(r['payment_instructions'])
+        if r['api_version'] == '1.0':
+            txn.payment_id = r['payment_id']
+            txn.short_payment_id = r['short_payment_id']
+            txn.payment_status = r['payment_status']
+            txn.creation_date = parse_date(r['creation_date'], ignoretz=True)
+            txn.expiration_date = parse_date(r['expiration_date'], ignoretz=True)
+            txn.updated_date = datetime.now()
+            txn.product_information = json.dumps(r['product_information'])
+            txn.payment_instructions = json.dumps(r['payment_instructions'])
+        elif r['api_version'] == '1.1':
+            txn.payment_id = r['id']
+            txn.short_payment_id = r['short_id']
+            txn.payment_status = r['status'].upper()
+            txn.creation_date = datetime.fromtimestamp(int(r['created']))
+            txn.expiration_date = datetime.fromtimestamp(int(r['exp_date']))
+            txn.updated_date = datetime.now()
+            order_info = r['order_info']
+            txn.product_information = json.dumps({
+                "product_id": order_info['order_id'],
+                "product_name": order_info['order_name'],
+                "product_price": order_info['order_price'],
+            })
+            instr = r['instructions']
+            details = instr['details']
+            txn.payment_instructions = json.dumps({
+                "step_1": instr['step_1'],
+                "step_2": instr['step_2'],
+                "step_3": instr['step_3'],
+                "note_confirmation": instr['note_confirmation'],
+                "description": instr['description'],
+                "note_expiration_date": instr['note_expiration_date'],
+                "details": {
+                    "payment_store": details['store'],
+                    "payment_amount": details['amount'],
+                    "bank_name": details['bank_name'],
+                    "bank_account_number": details['bank_account_number']
+                },
+                "note_extra_comition": instr['note_extra_comition'],
+            })
         txn.save()
 
     if txn.payment_status == 'PENDING':
@@ -94,17 +125,28 @@ def payment_selector_view(request, txn, template_name='compropago/payment_method
 @require_POST
 def web_hook_view(request):
     payload = json.loads(request.raw_post_data)
-    pay_id = payload['data']['object']['id']
-    txn = get_object_or_404(CompropagoTransaction, payment_id=pay_id)
+    if payload['api_version'] == '1.1':
+        payment_id = payload['id']
+    else: #assume 1.0
+        payment_id = payload['data']['object']['id']
+
+    txn = get_object_or_404(CompropagoTransaction, payment_id=payment_id)
     api = CompropagoAPI(settings.LFS_COMPROPAGO_PUBLIC_API_KEY)
-    res = api.verify_charge(pay_id)
-    if res['object'] == 'event' and res['type'] == 'charge.success':
-        txn.payment_status = res['type']
-        txn.verified = True
-        txn.save();
-        lfs_order_paid_signal.send({'order': txn.order})
-        if getattr(settings, 'LFS_SEND_ORDER_MAIL_ON_PAYMENT', False):
-            mail_utils.send_order_received_mail(txn.order)
-        txn.order.state = PAID
+    res = api.verify_charge(payment_id)
+    if 'type' in res:
+        pay_status = res['type']
+        if pay_status == 'charge.success':
+            txn.payment_status = pay_status
+            txn.verified = True
+            txn.save();
+            lfs_order_paid_signal.send({'order': txn.order})
+            if getattr(settings, 'LFS_SEND_ORDER_MAIL_ON_PAYMENT', False):
+                mail_utils.send_order_received_mail(txn.order)
+            txn.order.state = PAID
+        elif pay_status == 'error':
+            txn.payment_status = PAYMENT_FAILED
+            txn.order.state = PAYMENT_FAILED
+            txn.order.state_changed = datetime.now()
+        txn.save()
         txn.order.save()
     return HttpResponse(str(res), content_type="text/plain")
